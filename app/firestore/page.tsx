7821a16aca2e23df2f1cb05cca161a67ac243338
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-07-02 20:32:25 -0400

    refactor firestore page

--- app/firestore/page.tsx
+++ app/firestore/page.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { Suspense } from 'react';
+import Firestore from '@/components/Firestore';
+import { getKeywords } from '@/components/firestore/keywords';
+import { getFactions } from '@/components/firestore/factions';
 
-export default function FirestorePage() {
+export default async function FirestorePage() {
+  const [keywords, factions] = await Promise.all([
+    getKeywords(),
+    getFactions(),
+  ]); // [getKeywords(), getFactions()
+
+  const data = { keywords, factions };
+
   return (
-    <div className="mx-10 flex flex-col bg-gray-200">
-      <ol className="flex items-center justify-around">
-        {['factions', 'keywords'].map((category, index) => (
-          <li key={index} className="capitalize underline">
-            <Link href={`/firestore/${category}`}>{category}</Link>
-          </li>
-        ))}
-      </ol>
+    <div className="mx-10 flex flex-col">
+      <Suspense>
+        <Firestore data={data} />
+      </Suspense>
     </div>
   );
 }
