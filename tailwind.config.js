1e3068ea6a27fd03f4fc0e60afea12ff082750e0
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-07-02 20:26:04 -0400

    add headless ui selector package

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,5 +22,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@headlessui/tailwindcss')({ prefix: 'ui' }), // the prefix is customizable
+  ],
 };
