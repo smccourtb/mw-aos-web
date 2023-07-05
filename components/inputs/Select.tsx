33819e6f6cfe69da93998cfd61cff17a5b005319
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-07-04 22:37:22 -0400

    implement open/close icon, add optional placeholder prop

--- components/inputs/Select.tsx
+++ components/inputs/Select.tsx
@@ -1,86 +1,93 @@
 import React, { Fragment } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { useController, UseControllerProps } from 'react-hook-form';
-import { ChevronDownIcon, CheckIcon } from '@heroicons/react/solid';
+import {
+  ChevronDownIcon,
+  CheckIcon,
+  ChevronUpIcon,
+} from '@heroicons/react/solid';
 
 type SelectProps = {
   options: any[];
   multiple?: boolean;
+  placeholder?: string;
 };
 const Select = (props: UseControllerProps<any> & SelectProps) => {
   const { field } = useController(props);
   const { value, onChange } = field;
+
   const label = !props?.multiple
-    ? value?.name?.charAt(0).toUpperCase() + value?.name?.slice(1)
+    ? value?.charAt(0).toUpperCase() + value?.slice(1)
     : value
-        ?.map(
-          (v: { name: string }) =>
-            v?.name?.charAt(0).toUpperCase() + v?.name?.slice(1),
-        )
+        ?.map((v: string) => v?.charAt(0).toUpperCase() + v?.slice(1))
         .join(', ');
   return (
-    <div className="w-72">
-      <Listbox
-        value={value}
-        onChange={onChange}
-        by={'ref'}
-        multiple={props?.multiple}
-      >
-        <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">
-              {label || 'Select a faction'}
-            </span>
-            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronDownIcon
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            enter="transition ease-in duration-100"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-          >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {props.options.map((option) => (
-                <Listbox.Option
-                  key={option.ref}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 capitalize ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                    }`
-                  }
-                  value={option}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? 'font-medium' : 'font-normal'
-                        }`}
-                      >
-                        {option.name}
-                      </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+    <Listbox value={value} onChange={onChange} multiple={props?.multiple}>
+      {({ open }) => (
+        <>
+          <div className={'relative h-full w-full'}>
+            <Listbox.Button className="relative h-full w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <span className="block truncate">
+                {label || props.placeholder}
+              </span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                {open ? (
+                  <ChevronUpIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <ChevronDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                )}
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              enter="transition ease-in duration-100"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+            >
+              <Listbox.Options className="absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {props.options.map((option, index) => (
+                  <Listbox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 capitalize ${
+                        active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      }`
+                    }
+                    value={option}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {option}
                         </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>
+                        {selected ? (
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
   );
 };
 
---
+++ components/inputs/WeaponInput.tsx
@@ -0,0 +1,98 @@
+import Input from '@/components/inputs/Input';
+import { Control } from 'react-hook-form';
+import Select from '@/components/inputs/Select';
+import React from 'react';
+import Toggle from '@/components/inputs/Toggle';
+
+type WeaponInputProps = {
+  entryNumber: number;
+  control: Control<any>;
+};
+
+const WeaponInput = ({ entryNumber, control }: WeaponInputProps) => {
+  return (
+    <div className={'flex gap-2'}>
+      <Input
+        name={`weapons.${entryNumber}.name`}
+        control={control}
+        label={`Name`}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.type`}
+        placeholder={'Type'}
+        options={['missile', 'melee']}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.range`}
+        placeholder={'Range'}
+        options={['1', '2', '3', '4', '5', '6', '18', '12', '24', '30']}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.attacks`}
+        placeholder={'Attacks'}
+        options={[
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          'D3',
+          'D6',
+          '*',
+          'Models In Range(3)',
+        ]}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.toHit`}
+        placeholder={'To Hit'}
+        options={['1+', '2+', '3+', '4+', '5+', '6', '*']}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.toWound`}
+        placeholder={'To Wound'}
+        options={['1+', '2+', '3+', '4+', '5+', '6']}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.rend`}
+        placeholder={'Rend'}
+        options={['-1', '-2', '-3', '-']}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Select
+        name={`weapons.${entryNumber}.stats.damage`}
+        placeholder={'Damage'}
+        options={[
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          'D3',
+          'D6',
+          '*',
+          'Target Unit Count',
+          'Special',
+        ]}
+        control={control}
+        rules={{ required: true }}
+      />
+      <Toggle name={`weapons.${entryNumber}.required`} control={control} />
+    </div>
+  );
+};
+
+export default WeaponInput;
