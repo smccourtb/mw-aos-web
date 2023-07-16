bbe10872bf56d0507903701e861c27731c5a8fa6
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-07-16 13:39:03 -0400

    create functions for retrieving battlepack(s) from firestore

---
+++ firestore/battlepacks.ts
@@ -0,0 +1,25 @@
+import { db } from '@/firebase/serverFirebaseApps';
+import { Battlepack } from '@/firestore/types';
+
+export const getBattlePacks = async () => {
+  const response = await db.collection('battlepacks').get();
+  if (response.empty) {
+    console.log('No matching documents.');
+    return [];
+  }
+  const battlepacks: Battlepack[] = [];
+  response.forEach((doc) => {
+    battlepacks.push(doc.data() as Battlepack);
+  });
+
+  return battlepacks;
+};
+
+export const getBattlePack = async (id: string) => {
+  const response = await db.collection('battlepacks').doc(id).get();
+  if (!response.exists) {
+    console.log('No matching documents.');
+    return null;
+  }
+  return response.data() as Battlepack;
+};
