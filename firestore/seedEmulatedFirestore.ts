16e3e206e7ad020c89e6621cb226eaf64a14ab32
Shawnathan McCourt <shawnmccourtb@gmail.com>
2023-07-18 00:13:09 -0400

    create initial phase mock data

--- firestore/seedEmulatedFirestore.ts
+++ firestore/seedEmulatedFirestore.ts
@@ -4,6 +4,11 @@ import { seraphon } from '@/mocks/factions/seraphon';
 import contestOfGenerals from '@/mocks/battlepacks/contestOfGenerals.json';
 import enhancements from '@/mocks/enhancements/enhancements.json';
 import heroPhase from '@/mocks/phases/hero.json';
+import movementPhase from '@/mocks/phases/movement.json';
+import shootingPhase from '@/mocks/phases/shooting.json';
+import chargePhase from '@/mocks/phases/charge.json';
+import combatPhase from '@/mocks/phases/combat.json';
+import battleshockPhase from '@/mocks/phases/battleshock.json';
 
 export const seedEmulatedFirestore = async () => {
   const seedRef = await db.collection('seeded').get();
@@ -39,10 +44,19 @@ const setUpFactions = async () => {
 };
 
 const setUpPhases = async () => {
-  const collection = 'phases';
+  const phases = [
+    heroPhase,
+    movementPhase,
+    shootingPhase,
+    chargePhase,
+    combatPhase,
+    battleshockPhase,
+  ];
   const batch = db.batch();
-  const docRef = db.collection(collection).doc();
-  batch.set(docRef, heroPhase);
+  phases.forEach((phase) => {
+    const docRef = db.collection(`phases`).doc(phase.name);
+    batch.set(docRef, phase);
+  });
   await batch.commit();
   return;
 };
